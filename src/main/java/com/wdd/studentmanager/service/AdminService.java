d4bf4f8574862f3a7ebbc66dcf073663651493b2
Sunny-you <71618916+Sunny-you@users.noreply.github.com>
2020-11-18 15:37:40 +0800

    开发完成 第一次提交

---
+++ src/main/java/com/wdd/studentmanager/service/AdminService.java
@@ -0,0 +1,11 @@
+package com.wdd.studentmanager.service;
+
+import com.wdd.studentmanager.domain.Admin;
+
+public interface AdminService {
+
+    Admin findByAdmin(Admin admin);
+
+
+    int editPswdByAdmin(Admin admin);
+}
